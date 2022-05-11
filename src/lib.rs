e35e08809b68f9b6fb042ac9b6ebedd3b1fc54f9
Ben Jones <ben@reticent-monolith.com>
2022-05-11 10:47:14 +0100

    main working with ignore

--- src/lib.rs
+++ src/lib.rs
@@ -1,70 +1,33 @@
 pub mod grep {
-    pub fn find(pattern: &str, text: &str) -> Vec<String> {
-        let mut found_lines: Vec<String> = Vec::new();
-        for line in text.split('\n') {
-            if line.contains(pattern) {
-                found_lines.push(String::from(line))
+    pub fn find(pattern: &str, text: &str) -> bool {
+        text.contains(pattern)
+    }
+
+    pub fn find_ignore_case(pattern: &str, text: &str) -> bool {
+        text.to_lowercase().contains(&pattern.to_lowercase())
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+        #[test]
+        fn test_find() {
+            let tests = [
+                ("This is a test sentence.", "test", true),
+                ("This is a sentence", "test", false),
+                ("This is a Test sentence.", "test", false),
+            ];
+            for test in &tests {
+                assert_eq!(find(test.1, test.0), test.2);
             }
         }
-        found_lines
-    }
 
-    pub fn find_ignore_case(pattern: &str, text: &str) -> Vec<String> {
-        let mut found_lines: Vec<String> = Vec::new();
-        for line in text.split('\n') {
-            if line.to_lowercase().contains(&pattern.to_lowercase()) {
-                found_lines.push(String::from(line))
+        #[test]
+        fn test_find_ignore_case() {
+            let tests = [("This is a Test sentence.", "test", true)];
+            for test in &tests {
+                assert_eq!((find_ignore_case(test.1, test.0)), test.2);
             }
         }
-        found_lines
     }
 }
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn test_find() {
-	let tests = [
-            (
-		"This is a test sentence.\n",
-		"test",
-		vec!["This is a test sentence."],
-            ),
-            (
-		"This is a test sentence.\nAnd this is another test sentence.\n",
-		"test",
-		vec![
-                    "This is a test sentence.",
-                    "And this is another test sentence.",
-		],
-            ),
-            ("There should be no match here", "test", vec![]),
-            (
-		"This is a test sentence.\nThis is a sentence without a match.\n",
-		"test",
-		vec!["This is a test sentence."],
-            ),
-            (
-		"This is a test sentence.\nThis is another Test sentence.\n",
-		"test",
-		vec!["This is a test sentence."],
-            ),
-	];
-	for test in &tests {
-            assert_eq!(find(test.1, test.0), test.2);
-	}
-    }
-    
-    #[test]
-    fn test_find_ignore_case() {
-	let tests = [(
-            "This is a test sentence.\nThis is another Test sentence.\n",
-            "test",
-            vec!["This is a test sentence.", "This is another Test sentence."],
-	)];
-	for test in &tests {
-            assert_eq!((find_ignore_case(test.1, test.0)), test.2);
-	}
-    }
-}
-
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,39 @@
 use grep::grep;
+use std::env;
+use std::fs;
+use std::io;
+use std::io::BufRead;
 
-fn main() {
-    let sentence = "This is a test sentence.\nThis is also a test sentence.\n";
-    let result = grep::find("test", &sentence);
-    for r in &result {
-        println!("{}", r);  
+fn main() -> Result<(), String> {
+    // get pattern and filename from args
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 3 {
+	return Err(String::from("Invalid args, need at least a pattern and file"));
     }
+    let mut ignore = false;
+    if args.len() > 3 && args[3] == "ignore" {
+	ignore = true;
+    }
+    // use a function pointer to allow ignore case to call different fn
+    let mut func: fn(&str, &str) -> bool = grep::find;
+    if ignore { func = grep::find_ignore_case; }
+    // read file to a string or set up an iterator
+    let file = match fs::File::open(&args[2]) {
+	Ok(file) => file,
+	Err(e) => {return Err(e.to_string());}
+    };
+    let reader = io::BufReader::new(file);
+    let mut lines = reader.lines();
+    loop {
+	match lines.next() {
+	    Some(line) => {
+		let unwrapped = &line.unwrap();
+		if func(&args[1], &unwrapped) {
+		    println!("{}", &unwrapped)
+		}
+	    },
+	    None => break
+	}
+    }
+    Ok(())
 }
