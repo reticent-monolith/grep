d55f2492ed14cda96e8e8e7d11b74cd9ff7ab4bd
Ben Jones <ben@reticent-monolith.com>
2022-05-12 15:44:35 +0100

    formatting

--- src/main.rs
+++ src/main.rs
@@ -7,23 +7,27 @@ use std::io::BufRead;
 fn main() -> Result<(), String> {
     let args = grep::Args::parse(env::args().collect())?;
     let mut func: fn(&str, &str) -> bool = grep::find;
-    if args.ignore { func = grep::find_ignore_case; }
+    if args.ignore {
+        func = grep::find_ignore_case;
+    }
     let file = match fs::File::open(args.filename) {
-	Ok(file) => file,
-	Err(e) => {return Err(e.to_string());}
+        Ok(file) => file,
+        Err(e) => {
+            return Err(e.to_string());
+        }
     };
     let reader = io::BufReader::new(file);
     let mut lines = reader.lines();
     loop {
-	match lines.next() {
-	    Some(line) => {
-		let unwrapped = &line.unwrap();
-		if func(&args.pattern, &unwrapped) {
-		    println!("{}", &unwrapped)
-		}
-	    },
-	    None => break
-	}
+        match lines.next() {
+            Some(line) => {
+                let unwrapped = &line.unwrap();
+                if func(&args.pattern, &unwrapped) {
+                    println!("{}", &unwrapped)
+                }
+            }
+            None => break,
+        }
     }
     Ok(())
 }
